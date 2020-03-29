ba2440ec1fc920a897e81a9442de47bd85c3666b
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2020-03-29 21:10:19 +0530

    Add files via upload

--- Allocation.cpp
+++ Allocation.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(){
-    int T, N, B, i, x, y;
-    cin>>T;
-    for(x=1; x<=T; x++){
-        cin>>N>>B;
-        int A[N];
-        for(i=0; i<N; i++)
-            cin>>A[i];
-        sort(A, A+N);
-        for(i=0; i<N && B-A[i]>=0; i++)
-            B=B-A[i];
-        y=i;
-        cout<<"Case #"<<x<<": "<<y<<"\n";
-    }
-    return 0;
+	int T, N, B, i, x, y;
+	cin>>T;
+	for(x=1; x<=T; x++){
+		cin>>N>>B;
+		int A[N];
+		for(i=0; i<N; i++)
+			cin>>A[i];
+		sort(A, A+N);
+		for(i=0; i<N && B-A[i]>=0; i++)
+			B=B-A[i];
+		y=i;
+		cout<<"Case #"<<x<<": "<<y<<"\n";
+	}
+	return 0;
 }
