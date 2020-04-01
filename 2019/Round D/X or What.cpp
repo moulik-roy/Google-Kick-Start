9aa3e02e90e91acd3dd738871627d293d7038ec7
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2020-04-01 13:56:25 +0530

    Add files via upload

---
+++ 2019/Round D/X or What.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+	int T, N, Q, i, x, y, P, V, sum;
+	cin>>T;
+	for(x=1; x<=T; x++){
+		cin>>N>>Q;
+		int A[N];
+		set <int> positions;
+		for(i=0, sum=0; i<N; i++){
+			cin>>A[i];
+			A[i]=__builtin_popcount(A[i]);
+			sum=sum+A[i];
+			if(A[i]%2!=0)
+				positions.insert(i);
+		}
+		cout<<"Case #"<<x<<": ";
+		while(Q--){
+			cin>>P>>V;
+			sum=sum-A[P];
+			A[P]=__builtin_popcount(V);
+			sum=sum+A[P];
+			if(positions.find(P)!=positions.end())
+				positions.erase(P);
+			if(A[P]%2!=0)
+				positions.insert(P);
+			if(sum%2==0)
+				y=N;
+			else
+				y=max(N-1-*positions.begin(), *positions.rbegin());
+			cout<<y<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
