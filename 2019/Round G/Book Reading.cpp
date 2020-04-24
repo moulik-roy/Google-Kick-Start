742bec2b26ba4deea5cd3efac7f25e9f911238ee
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2020-04-24 16:49:33 +0530

    Add files via upload

--- 2019/Round G/Book Reading.cpp
+++ 2019/Round G/Book Reading.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
 
 int main(){
@@ -8,21 +7,23 @@ int main(){
 	cin>>T;
 	for(x=1; x<=T; x++){
 		cin>>N>>M>>Q;
-		vector <long int> pages(N+1, 1);
-		map <long int, long int> read;
+		vector <long int> pages(N+1, 1), read(N+1, -1);
 		for(i=0, y=0; i<M; i++){
 			cin>>P;
 			pages[P]=0;
 		}
 		while(Q--){
 			cin>>R;
-			if(M!=N && read.find(R)==read.end()){
-				for(i=R; i<=N; i+=R){
-					if(pages[i]==1)
-						read[R]++;
+			if(read[R]==-1){
+				read[R]=0;
+				if(M!=N){
+					for(i=R; i<=N; i+=R){
+						if(pages[i]==1)
+							read[R]++;
+					}
 				}
 			}
-			y=y+read[R];
+			y+=read[R];
 		}
 		cout<<"Case #"<<x<<": "<<y<<"\n";
 	}
